526f1f1ec8f48447b0aec7286f27609b14caf23b
Kailey Yang <kaileyy@mit.edu>
2022-05-04 01:50:10 -0400

    add restrictions for vegetarian + vegan levels

--- src/Scene/MainMenu.gd
+++ src/Scene/MainMenu.gd
@@ -5,25 +5,120 @@ extends Control
 # var a = 2
 # var b = "text"
 
-var foods 
+var badFoods 
+var game = load("res://Scene/Level.tscn").instance()
 
 # Called when the node enters the scene tree for the first time.
 func _on_RegularLevel_pressed():
-	print('hi')
-	var game = load("res://Scene/Level.tscn").instance()
-	foods = []
-	# is this right?
-	game.init(foods)
+	badFoods = []
+	game.init(badFoods)
 	self.hide()
 	get_tree().change_scene("res://Scene/Level.tscn")
 	
-
-func _on_HardLevel_pressed():
-	# TODO ADD FOODS
-	foods = ['Cheeseburger']
-	var game = load("res://Scene/Level.tscn").instance()
-	game.init(foods)
-	
+func _on_VeganLevel_pressed():
+	badFoods = ['Cheeseburger',
+			 'Brownie',
+			 'Oatmeal Raisin Cookie',
+			 'Whole Milk',
+			 'Milkshake',
+			 'Omelette',
+			 'Pancakes',
+			 'Pizza',
+			 'Fried Chicken',
+			 'Chicken Curry',
+			 'Shawarma',
+			 'Vanilla Ice Cream',
+			 'Grilled Cheese Sandwich',
+			 'Starbucks Mocha Frappe',
+			 'Waffles',
+			 'Sushi Rolls',
+			 'Chocolate Cupcake',
+			 'Chocolate Chip Muffin',
+			 'Garlic Bread',
+			 'Pink Sprinkled Donut',
+			 'Chicken Strips',
+			 'Chicken Shish Kebab',
+			 'Coffee Latte',
+			 'Cheesecake',
+			 'Mac and Cheese',
+			 'Ribeye Steak',
+			 'French Toast',
+			 'Chili',
+			 'Burrito',
+			 'Pho',
+			 'Pecan Pie',
+			 'Corn Flakes with Milk',
+			 'Biscuit with Gravy',
+			 'Bagel with Cream Cheese',
+			 'Gyro',
+			 'Chicken Caesar Wrap',
+			 'Fish Tacos',
+			 'Beef Fried Rice',
+			 'Beef Jerky',
+			 'Banana Bread',
+			 'Enchiladas',
+			 'Huevos Rancheros',
+			 'Hot Cheetos',
+			 'Apple Crisp',
+			 'Tiramisu',
+			 'Cinnamon roll',
+			 'Grits',
+			 'Scrambled Eggs',
+			 'Pop Tarts',
+			 'Quiche',
+			 'McMuffin',
+			 'Oreos',
+			 'Nachos & Cheese',
+			 'Spaghetti & Meatballs',
+			 'Lasagna',
+			 'Salmon',
+			 'Grilled Shrimp',
+			 'Chicken Noodle Soup',
+			 'Meatloaf',
+			 'Pad Thai',
+			 'Pork Fried Rice',
+			 'Turkey Sandwich',
+			 'Hot Dog',
+			 'Sloppy Joe Sandwich',
+			 'Ham Sandwich',
+			 'Poke Bowl',
+			 'Quesadilla']
+	game.init(badFoods)
 	self.hide()
 	get_tree().change_scene("res://Scene/Level.tscn")
+	pass # Replace with function body.
 
+
+func _on_VegetarianLevel_pressed():
+	badFoods = ['Cheeseburger',
+		 'Fried Chicken',
+		 'Chicken Curry',
+		 'Shawarma',
+		 'Sushi Rolls',
+		 'Chicken Strips',
+		 'Chicken Shish Kebab',
+		 'Ribeye Steak',
+		 'Chili',
+		 'Burrito',
+		 'Pho',
+		 'Gyro',
+		 'Chicken Caesar Wrap',
+		 'Fish Tacos',
+		 'Beef Fried Rice',
+		 'Beef Jerky',
+		 'Spaghetti & Meatballs',
+		 'Lasagna',
+		 'Salmon',
+		 'Grilled Shrimp',
+		 'Chicken Noodle Soup',
+		 'Meatloaf',
+		 'Pad Thai',
+		 'Pork Fried Rice',
+		 'Turkey Sandwich',
+		 'Hot Dog',
+		 'Sloppy Joe Sandwich',
+		 'Ham Sandwich',
+		 'Poke Bowl']
+	game.init(badFoods)
+	self.hide()
+	get_tree().change_scene("res://Scene/Level.tscn")
